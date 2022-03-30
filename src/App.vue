a56e695a9c36c947adb0582708404d9326af1f05
filway <filway@126.com>
2022-03-31 00:38:57 +0800

    调试云构建支持yarn安装依赖

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import {
   <el-row class="mb-4">
     <el-button>Default</el-button>
     <el-button type="primary">Primary</el-button>
+    <el-link type="primary">调试云构建使用yarn安装</el-link>
     <el-button type="success">Success</el-button>
     <el-button type="info">Info</el-button>
     <el-button type="warning">Warning</el-button>
